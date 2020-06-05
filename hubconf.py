407983ca4007964df9d359585e01624d2cd1de46
Rene Ranftl <ranftlr@gmail.com>
2020-06-05 14:10:03 +0200

    Fixed link to weight

--- hubconf.py
+++ hubconf.py
@@ -13,7 +13,7 @@ def MiDaS(pretrained=True, **kwargs):
     model = MidasNet()
 
     if pretrained:
-        checkpoint = "https://drive.google.com/file/d/1nqW_Hwj86kslfsXR7EnXpEWdO2csz1cC"
+        checkpoint = "https://github.com/intel-isl/MiDaS/releases/download/v2/model.pt"
         state_dict = torch.hub.load_state_dict_from_url(checkpoint, progress=True)
         model.load_state_dict(state_dict)
 
