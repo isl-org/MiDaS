bf9061dfd04d58c204549a057da43fac046e04e0
Rene Ranftl <ranftlr@gmail.com>
2020-06-16 09:41:45 +0200

    Expose default transform in hubconf

--- hubconf.py
+++ hubconf.py
@@ -5,7 +5,6 @@ import torch
 from midas.midas_net import MidasNet
 
 
-
 def MiDaS(pretrained=True, **kwargs):
     """ # This docstring shows up in hub.help()
     MiDaS model for monocular depth estimation
@@ -24,3 +23,29 @@ def MiDaS(pretrained=True, **kwargs):
         model.load_state_dict(state_dict)
 
     return model
+
+
+def transforms():
+    import cv2
+    from torchvision.transforms import Compose
+    from midas.transforms import Resize, NormalizeImage, PrepareForNet
+    from midas import transforms
+
+    transforms.default_transform = Compose(
+        [
+            lambda img: {"image": img / 255.0},
+            Resize(
+                384,
+                384,
+                resize_target=None,
+                keep_aspect_ratio=True,
+                ensure_multiple_of=32,
+                resize_method="upper_bound",
+                image_interpolation_method=cv2.INTER_CUBIC,
+            ),
+            NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            PrepareForNet(),
+            lambda sample: torch.from_numpy(sample["image"]).unsqueeze(0),
+        ]
+    )
+    return transforms
