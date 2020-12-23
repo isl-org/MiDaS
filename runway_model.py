8687b795a8a90bc3b75c49b200a97b954444482d
ak9250 <ahsen.khaliq@gmail.com>
2020-12-23 16:02:26 -0500

    torch hub setting

--- runway_model.py
+++ runway_model.py
@@ -2,22 +2,21 @@ import runway
 import cv2
 import torch
 import numpy as np
-
-@runway.setup(options={'checkpoint_dir': runway.file(extension='.pt', description="checkpoint file"),})
-def setup(opts):
-
-    midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
-    # midas = torch.load(opts['checkpoint_dir'])
-    return midas
+import download_checkpoint
 
 
-@runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),}, outputs={'image': runway.image(description='output image containing the translated result')})
+@runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),'large': boolean(default=True, description='use large model'),}, outputs={'image': runway.image(description='output image containing the translated result')})
 def translate(midas, inputs):
     cv_image = np.array(inputs['source_imgs']) 
     img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-    transform = midas_transforms.small_transform
-    input_batch = transform(img)
+    if inputs['large']:
+        transform = download_checkpoint.transforml
+        input_batch = transform(img)
+        midas = download_checkpoint.midasl
+    else:     
+        transform = download_checkpoint.transformS
+        input_batch = transform(img)
+        midas = download_checkpoint.midasS
 
     with torch.no_grad():
         prediction = midas(input_batch)
