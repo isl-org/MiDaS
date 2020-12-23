de3370a5c3c5598c66d5ab607d9b4639e5aba19b
ak9250 <ahsen.khaliq@gmail.com>
2020-12-23 13:41:39 -0500

    small model

--- runway_model.py
+++ runway_model.py
@@ -1,32 +1,22 @@
 import runway
-import torch
 import cv2
 import torch
 
 @runway.setup(options={'checkpoint_dir': runway.file(extension='.pt', description="checkpoint file"),})
 def setup(opts):
-    midas = torch.hub.load("intel-isl/MiDaS", "MiDaS")
-    midas.eval()
-    use_large_model = True
 
-    if use_large_model:
-        midas = torch.hub.load("intel-isl/MiDaS", "MiDaS")
-    else:
-        midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
+ 
+    midas = torch.load(opts['checkpoint_dir'])
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     midas.to(device)
     midas.eval()
     midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-
-    if use_large_model:
-        transform = midas_transforms.default_transform
-    else:
-        transform = midas_transforms.small_transform
-    return model
+    transform = midas_transforms.small_transform
+    return transform, midas
 
 
 @runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),}, outputs={'image': runway.image(description='output image containing the translated result')})
-def translate(model, inputs):
+def translate(transform,midas, inputs):
     img = cv2.imread(inputs['source_imgs'])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
