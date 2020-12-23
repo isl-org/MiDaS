8d1a0ce048abc8c3e011f5232b0bd78cb247dd65
ak9250 <ahsen.khaliq@gmail.com>
2020-12-23 14:30:18 -0500

    working version dev

--- runway_model.py
+++ runway_model.py
@@ -1,26 +1,23 @@
 import runway
 import cv2
 import torch
+import numpy as np
 
 @runway.setup(options={'checkpoint_dir': runway.file(extension='.pt', description="checkpoint file"),})
 def setup(opts):
 
- 
-    midas = torch.load(opts['checkpoint_dir'])
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    midas.to(device)
-    midas.eval()
-    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-    transform = midas_transforms.small_transform
-    return transform, midas
+    midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
+    # midas = torch.load(opts['checkpoint_dir'])
+    return midas
 
 
 @runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),}, outputs={'image': runway.image(description='output image containing the translated result')})
-def translate(transform,midas, inputs):
-    img = cv2.imread(inputs['source_imgs'])
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    input_batch = transform(img).to(device)
+def translate(midas, inputs):
+    cv_image = np.array(inputs['source_imgs']) 
+    img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+    transform = midas_transforms.small_transform
+    input_batch = transform(img)
 
     with torch.no_grad():
         prediction = midas(input_batch)
@@ -32,12 +29,9 @@ def translate(transform,midas, inputs):
             align_corners=False,
         ).squeeze()
     
-    output = prediction.cpu().numpy()
+    output = prediction.numpy()
    
     return output
-
-
-
-
+    
 if __name__ == '__main__':
     runway.run(port=8889)
